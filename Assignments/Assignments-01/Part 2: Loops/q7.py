f094cd0d781312c10aba3b1b1b9962718d49d7f9
shery7310 <box_99@live.com>
2024-11-08 18:48:28 +0500

    Improved Factorial Logic

--- Assignments/Assignments-01/Part 2: Loops/q7.py
+++ Assignments/Assignments-01/Part 2: Loops/q7.py
@@ -2,13 +2,15 @@
 
 num = int(input("Enter Any Number:\n"))
 
-counter = 2
+counter = 2 # starting from 2 so multiplying actually starts from 2 instead of 1 
 fact = 1
-while counter < num:
+while counter < num + 1: # for example user entered 5, this will multiply like 1 x 2 x 3 x 4 x 5 which is 120
     fact *= counter
     counter += 1
 
-print(f"The factorial is: {fact*num}") 
+print(f"The factorial is: {fact}") 
+
+
 
 '''
 Here's how we can achieve the same thing iterating reversely i.e. 5, 4, 3, 2, 1...
