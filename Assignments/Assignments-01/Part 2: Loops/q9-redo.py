338a25efa08bdd002f387f13190814acf0324825
shery7310 <box_99@live.com>
2024-11-09 00:50:05 +0500

    Did the Fibonacci Series Again without the need of a list and with logic that I understand better

--- Assignments/Assignments-01/Part 2: Loops/q9-redo.py
+++ Assignments/Assignments-01/Part 2: Loops/q9-redo.py
@@ -1,18 +1,23 @@
-# Write a program to print the first 10 Fibonacci numbers.
-# 1 1 2 3 5 8 13 21 34 55
+''' 0 1 1 2 3 5 8 13 21 34 55
+Some People say that Fibonacci Series starts with 0 and some say it starts with 1.
+We are going to start it with 1
+Each Next number is the sum of previous/preceding 2 numbers
+In this implementation I am using for loop to print fibonacci series
+there is no need to use list but i am using it so we actually see how varaible sum is being created
+in each iteration
+'''
 
-# In this approach we are swapping two values with one and another
+a, b = 0, 1
+fibonacci_10 = [1]
+# print(f"{b}", end=" ")
+for i in range(9):
+    sum = a + b # a = 0 + b = 1
+    print(f"(a:{a} + b:{b}) = {sum}") 
+    print(sum, end=" ") # 1 2 3 5 8 13 21 34 55 would have given us entire series without list
+    fibonacci_10.append(sum)
+    a = b
+    b = sum
 
-num1 = 0
-num2 = 1
-
-num = 0
-
-while num < 9:
-    num3 = num1 + num2
-    print(num3, end=" ")
-    if num == 0:
-        print(num3, end=" ")
-    num1 = num2
-    num2 = num3
-    num += 1
+print("Actual Fibonacci Series:")
+for elem in fibonacci_10:
+    print(elem, end=" ")
