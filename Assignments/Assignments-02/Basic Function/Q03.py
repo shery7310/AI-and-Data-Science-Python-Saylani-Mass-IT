bfa5939ec2e6b82a77171eb28c95a1351939c9f5
shery7310 <box_99@live.com>
2024-11-25 20:10:48 +0500

    Factorial using recursion

---
+++ Assignments/Assignments-02/Basic Function/Q03.py
@@ -0,0 +1,15 @@
+num = int(input("enter num:\n"))
+
+''' Equivalent Loop Structure
+fact = num
+for i in range(num - 1, 1, -1):
+    fact *= i
+'''
+
+def rec_fact(number):
+    if number == 1:
+        return 1
+    
+    return number * rec_fact(number - 1)
+
+print(rec_fact(num))
