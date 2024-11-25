43c7484cdd8c7fe1a2b2b6bad0b675af079f618c
shery7310 <box_99@live.com>
2024-11-25 19:22:45 +0500

    Added rename_files.fish script

---
+++ Assignments/rename_files.fish
@@ -0,0 +1,5 @@
+for file in q*.py
+    set number (string replace .py '' $file)   # Remove .py extension
+    set number (string replace q '' $number)  # Remove 'q' prefix
+    mv "$file" "Conditional Statements Q$number.py"
+end
