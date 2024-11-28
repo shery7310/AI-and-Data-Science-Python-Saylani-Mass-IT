314d0669b05526ed9eafa2f2a6ef2c8ee1fc38b7
shery7310 <box_99@live.com>
2024-11-28 17:18:38 +0500

    Changed Logic So if a vowels get repeated that is also stored

--- Assignments/Assignments-02/Basic Function/Q06.py
+++ Assignments/Assignments-02/Basic Function/Q06.py
@@ -4,25 +4,25 @@ these are vowels: a, e, i, o, u """
 vowels_in_each_word = {}
 sentence = input("Enter A Sentence:\n").split(" ")
 
-def is_vowel(string):
+def is_vowel(user_input_sentence):
 
     vowels = ('a', 'e', 'i', 'o', 'u')
-    for word_num in range(len(string)):
+    for word_num in range(len(user_input_sentence)):
         vowel_ch = ""
-        for vowel in vowels:
-
-            if vowel in string[word_num]:
-                vowel_ch += vowel
+        for ch in user_input_sentence[word_num]:
+            if ch in vowels:
+                vowel_ch += ch
                 vowels_in_each_word[word_num] = vowel_ch
 
 
-def show_vowels(string):
 
-    for word_num in range(len(string)):
+def show_vowels(user_input_sentence):
+
+    for word_num in range(len(user_input_sentence)):
         if word_num in vowels_in_each_word:
-            print(f'The word "{string[word_num]}" has {len(vowels_in_each_word[word_num])} vowel(s) which are: {vowels_in_each_word[word_num]}')
+            print(f'The word "{user_input_sentence[word_num]}" has {len(vowels_in_each_word[word_num])} vowel(s) which is/are: {vowels_in_each_word[word_num]}')
         else:
-            print(f'The word "{string[word_num]}" has no vowels')
+            print(f'The word "{user_input_sentence[word_num]}" has no vowels')
 
 is_vowel(sentence)
 show_vowels(sentence)
