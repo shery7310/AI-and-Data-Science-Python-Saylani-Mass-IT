f89b88aeac453b2c31804bb42007c43456fe08c5
shery7310 <box_99@live.com>
2024-12-10 22:40:25 +0500

    Added Q15

--- Assignments/Assignments-03/Advanced Dictionary Usage/Q15.py
+++ Assignments/Assignments-03/Advanced Dictionary Usage/Q15.py
@@ -1,3 +1,3 @@
 # Write a Python function to check if two dictionaries are identical (contain the same key-value pairs).
-# This is Question No. 15:
-# Q11 Create Dictionary from Tuples.py
+# This is the one missing question:
+# https://github.com/shery7310/AI-and-Data-Science-Python-Saylani-Mass-IT-SMIT/blob/main/Assignments/Assignments-03/Advanced%20Dictionary%20Usage/Q11%20Create%20Dictionary%20from%20Tuples.py
